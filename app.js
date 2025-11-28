58c15027b66b1b35fe37dfab5acc02a60b8b655b
tahsin1453 <40043963+tahsin1453@users.noreply.github.com>
2025-11-28 17:40:19 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
-import './crons.js';
-console.log("AutoSEO Backend running...");
+import express from "express";
+import cron from "cron";
+import job from "./crons.js";
+
+const app = express();
+const PORT = process.env.PORT || 10000;
+
+app.get("/", (req,res)=>res.send("AutoSEO Backend Running"));
+
+job.start();
+
+app.listen(PORT, ()=> console.log("Backend running on "+PORT));
